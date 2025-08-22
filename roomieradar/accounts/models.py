49bebb1e9cc80867323d1cd8d7af5d40b8c90378
Niveditha kummetha <224g1a0563@srit.ac.in>
2025-08-22 22:06:53 +0530

    Update models.py

--- roomieradar/accounts/models.py
+++ roomieradar/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from base.models import BaseModel
+from roomieradar.base.models import BaseModel
 from django_countries.fields import CountryField
 
 class ShippingAddress(BaseModel):
